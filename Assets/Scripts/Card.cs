3f8d90ef15944156fbd1a0a3159386fb06b28359
Chris Smith <chris.smith@ahead.com>
2022-04-15 15:03:26 -0400

    Experiment with TextMesh, add card

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -7,11 +7,14 @@ namespace Blackjack
 {
     public class Card : MonoBehaviour
 {
-    public String fa
+    public string Face;
+    public string Suit;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        var text = GetComponentInChildren<TextMesh>();
+        text.text = string.Format("{0}\n of\n {1}", Face, Suit);
     }
 
     // Update is called once per frame
